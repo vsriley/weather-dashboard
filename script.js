673670d3002e7987c6224e4bdf850ca783262a32
Valerie Riley <valeriesriley@gmail.com>
2019-11-30 12:29:02 -0700

    added weather icon to title

--- script.js
+++ script.js
@@ -19,9 +19,34 @@ $("#searchCity").on("click", function(event) {
 
         var title = $('<h1>');
         var currentDate = moment().format('L');
+        var weatherIcon = $('<i>');
+
         console.log("currentDate: " + currentDate);
-        title.text(city + " " + currentDate);
+
+        if(response.weather[0].main === "Clouds"){
+            weatherIcon.attr("class", "fas fa-cloud");
+        } else if (response.weather[0].main === "Rain"){
+            weatherIcon.attr("class", "fas fa-cloud-showers-heavy");
+        } else if (response.weather[0].main === "Thunderstorm"){
+            weatherIcon.attr("class", "fas fa-bolt");
+        } else if (response.weather[0].main === "Drizzle"){
+            weatherIcon.attr("class", "fas fa-cloud-rain");
+        } else if (response.weather[0].main === "Snow"){
+            weatherIcon.attr("class", "fas fa-snowflake");
+        } else if (response.weather[0].main === "Mist" || response.weather[0].main === "Smoke" || response.weather[0].main === "Haze" || response.weather[0].main === "Dust" || response.weather[0].main === "Fog" || response.weather[0].main === "Sand" || response.weather[0].main === "Ash"){
+            weatherIcon.attr("class", "fas fa-smog");
+        } else if (response.weather[0].main === "Squall" || response.weather[0].main === "Tornado"){
+            weatherIcon.attr("class", "fas fa-wind");
+        } else if (response.weather[0].main === "Clear"){
+            weatherIcon.attr("class", "fas fa-sun");
+        }
+
+
+        title.text(city + " " + currentDate + " ");
+        title.append(weatherIcon);
         $(".cityTitle").append(title);
+
+        
 
 
     });
